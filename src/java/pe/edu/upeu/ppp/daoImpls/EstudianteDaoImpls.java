3a9eab65cb3cd21581367eda29edb0930f771d57
USER <USER@DESKTOP-S7300UH>
2024-07-04 02:58:44 -0500

    v2-base

--- src/java/pe/edu/upeu/ppp/daoImpls/EstudianteDaoImpls.java
+++ src/java/pe/edu/upeu/ppp/daoImpls/EstudianteDaoImpls.java
@@ -7,6 +7,7 @@ package pe.edu.upeu.ppp.daoImpls;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import pe.edu.upeu.ppp.config.Conexion;
 import pe.edu.upeu.ppp.dao.EstudianteDao;
@@ -43,22 +44,87 @@ public class EstudianteDaoImpls implements EstudianteDao {
 
     @Override
     public int actualizarEstudiante(EstudianteEntity e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int x = 0;
+        String SQL = "UPDATE estudiante SET codigo = ?, idpersona = ? WHERE idestudiante = ?";
+
+        try {
+            cx = Conexion.getConnection();
+            ps = cx.prepareStatement(SQL);
+            ps.setInt(1, e.getCodigo());
+            ps.setInt(2, e.getId_persona());
+            ps.setInt(3, e.getId_estudiante());
+
+            x = ps.executeUpdate();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+
+        return x;
     }
 
     @Override
     public int eliminarEstudiante(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int x = 0;
+        String SQL = "DELETE FROM estudiante WHERE idestudiante = ?";
+
+        try {
+            cx = Conexion.getConnection();
+            ps = cx.prepareStatement(SQL);
+            ps.setInt(1, id);
+            x = ps.executeUpdate();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+
+        return x;
     }
 
     @Override
     public EstudianteEntity leerEstudiante(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EstudianteEntity re = new EstudianteEntity();
+        String SQL = "SELECT * FROM estudiante WHERE idestudiante = ?";
+
+        try {
+            cx = Conexion.getConnection();
+            ps = cx.prepareStatement(SQL);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                re.setId_estudiante(rs.getInt("idestudiante"));
+                re.setCodigo(rs.getInt("codigo"));
+                re.setId_persona(rs.getInt("idpersona"));
+
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+
+        return re;
     }
 
     @Override
     public List<EstudianteEntity> leertodo() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<EstudianteEntity> lista = new ArrayList<>();
+        String SQL = "SELECT * FROM estudiante";
+
+        try {
+            cx = Conexion.getConnection();
+            ps = cx.prepareStatement(SQL);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                EstudianteEntity re = new EstudianteEntity();
+                re.setId_estudiante(rs.getInt("idestudiante"));
+                re.setCodigo(rs.getInt("codigo"));
+                re.setId_persona(rs.getInt("idpersona"));
+                lista.add(re);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+
+        return lista;
     }
 
 }
